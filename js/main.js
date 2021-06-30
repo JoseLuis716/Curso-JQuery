c57412cdfe6b12aafd5acd8bdb72bf8f57c2aadd
jose.riveraa <jose.riveraa@SAF-STIC-JOSE>
2021-06-30 16:28:53 -0500

    Se agregó un cambiador de temas desde cero y sin logica de nada

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-$(document).ready(function () {
+$(document).ready(function() {
 
     //Slider
     $('.galeria').bxSlider({
@@ -9,39 +9,38 @@ $(document).ready(function () {
         slideWidth: 1200,
         responsive: true,
         pager: false //el pager hace que se muestren 3 puntos en medio 
-      });
+    });
 
-      //Posts
-      var posts = [
-          {
-              tittle: 'Tu y el titulo 1',
-              date: 'Publicado el dia '+ moment().format("dddd") + ', '+moment().date()+ ' de '+moment().format("MMMM")+' del '+moment().format("YYYY"),
-              content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat voluptatum, quam unde error nobis est praesentium reprehenderit accusamus doloribus autem at repudiandae expedita doloremque hic similique deleniti quo. Nostrum possimus architecto eaque delectus praesentium quibusdam temporibus blanditiis. Fuga reprehenderit quo, possimus repellat dolorem nam sit totam eveniet ex vero ipsam quas, laboriosam minima earum iusto adipisci laudantium officiis eius ratione ab molestias'
-          },
-          {
+    //Posts
+    var posts = [{
+            tittle: 'Tu y el titulo 1',
+            date: 'Publicado el dia ' + moment().format("dddd") + ', ' + moment().date() + ' de ' + moment().format("MMMM") + ' del ' + moment().format("YYYY"),
+            content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat voluptatum, quam unde error nobis est praesentium reprehenderit accusamus doloribus autem at repudiandae expedita doloremque hic similique deleniti quo. Nostrum possimus architecto eaque delectus praesentium quibusdam temporibus blanditiis. Fuga reprehenderit quo, possimus repellat dolorem nam sit totam eveniet ex vero ipsam quas, laboriosam minima earum iusto adipisci laudantium officiis eius ratione ab molestias'
+        },
+        {
             tittle: 'Tu y el titulo 2',
-            date: 'Publicado el dia '+ moment().format("dddd") + ', '+moment().date() + ' de '+moment().format("MMMM")+' del '+moment().format("YYYY"),
+            date: 'Publicado el dia ' + moment().format("dddd") + ', ' + moment().date() + ' de ' + moment().format("MMMM") + ' del ' + moment().format("YYYY"),
             content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat voluptatum, quam unde error nobis est praesentium reprehenderit accusamus doloribus autem at repudiandae expedita doloremque hic similique deleniti quo. Nostrum possimus architecto eaque delectus praesentium quibusdam temporibus blanditiis. Fuga reprehenderit quo, possimus repellat dolorem nam sit totam eveniet ex vero ipsam quas, laboriosam minima earum iusto adipisci laudantium officiis eius ratione ab molestias'
-          },
-          {
+        },
+        {
             tittle: 'Tu y el titulo 3',
-            date: 'Publicado el dia '+ moment().format("dddd") + ', '+moment().date() + ' de '+moment().format("MMMM")+' del '+moment().format("YYYY"),
+            date: 'Publicado el dia ' + moment().format("dddd") + ', ' + moment().date() + ' de ' + moment().format("MMMM") + ' del ' + moment().format("YYYY"),
             content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat voluptatum, quam unde error nobis est praesentium reprehenderit accusamus doloribus autem at repudiandae expedita doloremque hic similique deleniti quo. Nostrum possimus architecto eaque delectus praesentium quibusdam temporibus blanditiis. Fuga reprehenderit quo, possimus repellat dolorem nam sit totam eveniet ex vero ipsam quas, laboriosam minima earum iusto adipisci laudantium officiis eius ratione ab molestias'
-          },
-          {
+        },
+        {
             tittle: 'Tu y el titulo 4',
-            date: 'Publicado el dia '+ moment().format("dddd") + ', '+moment().date() + ' de '+moment().format("MMMM")+' del '+moment().format("YYYY"),
+            date: 'Publicado el dia ' + moment().format("dddd") + ', ' + moment().date() + ' de ' + moment().format("MMMM") + ' del ' + moment().format("YYYY"),
             content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat voluptatum, quam unde error nobis est praesentium reprehenderit accusamus doloribus autem at repudiandae expedita doloremque hic similique deleniti quo. Nostrum possimus architecto eaque delectus praesentium quibusdam temporibus blanditiis. Fuga reprehenderit quo, possimus repellat dolorem nam sit totam eveniet ex vero ipsam quas, laboriosam minima earum iusto adipisci laudantium officiis eius ratione ab molestias'
-          },
-          {
+        },
+        {
             tittle: 'Tu y el titulo 5',
-            date: 'Publicado el dia '+ moment().format("dddd") + ', '+moment().date() + ' de '+moment().format("MMMM")+' del '+moment().format("YYYY"),
+            date: 'Publicado el dia ' + moment().format("dddd") + ', ' + moment().date() + ' de ' + moment().format("MMMM") + ' del ' + moment().format("YYYY"),
             content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat voluptatum, quam unde error nobis est praesentium reprehenderit accusamus doloribus autem at repudiandae expedita doloremque hic similique deleniti quo. Nostrum possimus architecto eaque delectus praesentium quibusdam temporibus blanditiis. Fuga reprehenderit quo, possimus repellat dolorem nam sit totam eveniet ex vero ipsam quas, laboriosam minima earum iusto adipisci laudantium officiis eius ratione ab molestias'
-          } 
-      ];
-     
-      posts.forEach((item,index) => {
-        var post =`
+        }
+    ];
+
+    posts.forEach((item, index) => {
+        var post = `
         <article class="post">
                     <h2>${item.tittle}</h2>
                     <span class="date">${item.date}</span>
@@ -49,10 +48,20 @@ $(document).ready(function () {
                     <p>${item.content}.</p>
                     <a href="#" class="button-more">Leer mas</a>
                 </article> 
-        `; 
+        `;
         $("#posts").append(post);
-      });
+    });
 
+    //Selector de tema
+    var theme = $("#theme");
+    $("#to-green").click(function() {
+        theme.attr("href", "css/green.css");
+    });
+    $("#to-red").click(function() {
+        theme.attr("href", "css/red.css");
+    });
+    $("#to-blue").click(function() {
+        theme.attr("href", "css/blue.css");
+    });
 
- 
 });
